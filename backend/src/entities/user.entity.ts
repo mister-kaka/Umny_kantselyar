251afcc9fbb49b3ca18af538a2905dac99347981
Mariia Nachinova <mnachinova27@gmail.com>
2026-04-06 13:48:23 +0300

    Добавлена структура папок: auth, dashboard, db, entities, .env

---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,16 @@
+// import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity('users')
+// export class User {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   email: string;
+
+//   @Column({ name: 'password_hash' })
+//   passwordHash: string;
+
+//   @Column({ name: 'full_name' })
+//   fullName: string;
+// }
