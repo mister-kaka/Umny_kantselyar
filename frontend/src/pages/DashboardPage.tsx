4e5a6e4e7a96f2299492026e4f5bd60fe9975c40
MakaronniyMonstr3042 <efanovea@mail.ru>
2026-04-07 15:59:26 +0300

    Импортированы Sidebar и Card

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,4 +1,11 @@
+import Sidebar from "../components/Sidebar" 
+import Card from "../components/Card"
 const DashboardPage = () => {
-  return <div>Страница дашборда</div>;
-};
-export default DashboardPage;
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+
+}
+export default DashboardPage
