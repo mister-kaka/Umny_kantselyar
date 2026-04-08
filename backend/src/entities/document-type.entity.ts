de0a0326273c311cab93fbfd536ddda122c2061f
Mariia Nachinova <mnachinova27@gmail.com>
2026-04-08 10:38:06 +0300

    исправлена сущность DocumentType

--- backend/src/entities/document-type.entity.ts
+++ backend/src/entities/document-type.entity.ts
@@ -2,15 +2,15 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('document_types')
 export class DocumentType {
-  @PrimaryGeneratedColumn()
-  id!: number;
+    @PrimaryGeneratedColumn()
+    id!: number;
 
-  @Column({ length: 100 })
-  name!: string;
+    @Column({ length: 100 })
+    name!: string;
 
-  @Column({ length: 50, unique: true})
-  code!: string;
+    @Column({ length: 50, unique: true})
+    code!: string;
 
-  @Column({ name: 'description', type: 'text', nullable: true})
-  description!: string | null;
+    @Column({ name: 'description', type: 'text', nullable: true})
+    description!: string | null;
 }
