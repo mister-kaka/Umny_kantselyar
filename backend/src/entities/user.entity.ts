c194913671cabaabdad813773d41c4bd79626d06
Mariia Nachinova <mnachinova27@gmail.com>
2026-04-08 10:01:11 +0300

    Исправлена сущность User

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -5,13 +5,13 @@ export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ name: 'full_name' })
+  @Column({ name: 'full_name', length: 200})
   fullName!: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, length: 100 })
   email!: string;
 
-  @Column({ name: 'password_hash' })
+  @Column({ name: 'password_hash', length: 255 })
   passwordHash!: string;
 
   @Column({ name: 'role_id' })
@@ -20,7 +20,7 @@ export class User {
   @Column({ name: 'department_id', nullable: true })
   departmentId!: number | null;  
 
-  @Column({ default: 'active' })
+  @Column({ default: 'active', length: 20 })
   status!: string;
 
   @Column({ name: 'created_at', type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
