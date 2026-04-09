58bd151e0510f586796ac59cd2be6e3235c29685
Александра Москалева <parsivaasol@gmail.com>
2026-04-09 14:57:24 +0300

    обновлен файл app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';  
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //   import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />  
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
