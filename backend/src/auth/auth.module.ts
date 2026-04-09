cb7efbbf193446a842657e889a10c8f191f2093a
Mariia Nachinova <mnachinova27@gmail.com>
2026-04-09 13:36:26 +0300

    Зарегистрирована сущность User в TypeORM для модуля auth

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
   controllers: [AuthController]
 })
