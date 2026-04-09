a8724117e77638f89893095575eb6db02ac279b6
MakaronniyMonstr3042 <efanovea@mail.ru>
2026-04-09 03:37:57 +0300

    Импортирован Header

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,9 +1,12 @@
 import Sidebar from "../components/Sidebar" 
 import Card from "../components/Card"
+import Header from "../components/Header"
+import "../styles/global.css"
 const DashboardPage = () => {
   return (
-    <div>
+    <div className="body">
       <Sidebar/>
+      <Header/>
     </div>
   )
 
