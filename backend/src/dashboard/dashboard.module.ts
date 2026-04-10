b791c75411fa8edce71cf7eac8f6c2f81e398098
Mariia Nachinova <mnachinova27@gmail.com>
2026-04-10 12:50:24 +0300

    подключены сущности Document и DocumentRoute к DashboardModule

--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { Document } from '../entities/document.entity';
+import { DocumentRoute } from '../entities/document-route.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Document, DocumentRoute])],
   providers: [DashboardService],
   controllers: [DashboardController]
 })
