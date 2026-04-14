78e2812f46e8c9fa29582ab043a53e6ee5829fea
MakaronniyMonstr3042 <efanovea@mail.ru>
2026-04-14 20:50:18 +0300

    Избавление от всех Inline-стилей

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../styles/global.css"
 import { NavLink } from 'react-router-dom';
 import { useSidebar } from "../contexts/SidebarContexts";
+import "../styles/Dashboard.css"
 
 const Sidebar = () => {
   const { collapsed, toggleSidebar } = useSidebar();
@@ -18,29 +19,29 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className={`sidebar ${collapsed ? 'collapsed' : ''}`} style={{display:"grid"}}>
+    <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
       <div>
-        <button className="button-hide" style={{marginLeft: collapsed ? "18px" : "200px"}}
+        <button className={`button-hide ${collapsed ? 'collapsed' : ''}`}
         onClick={toggleSidebar}>
-          <img src="" className="Casual-icon" style={{justifySelf: "right"}} alt="" />
+          <img src="" className="Casual-icon" alt="" />
         </button>
         {!collapsed && (
-        <div style={{marginLeft: "18px", marginRight: "18px", marginBottom: "18px"}}>
+        <div className="Umny-cantselyar-text">
           <h3>
             Умный Канцеляр
           </h3>
-          <h6 className="item-label" style={{marginBottom: "0px", marginLeft: "0px", color: "var(--text-secondary)"}}>
+          <h6 className="AOD-text">
             Автоматизация обработки документов
           </h6>
         </div>
         ) || (
-        <div style={{marginLeft: "18px", marginRight: "18px", marginBottom: "18px"}}>
+        <div className="Umny-cantselyar-text">
           <h3>
             Ум. <br/>К.
           </h3>
         </div>
         )}
-        <div style={collapsed ? {marginTop: "24px"} : {marginTop: "23px"}}>
+        <div className={`items-margin-top ${collapsed ? "collapsed" : ""}`}>
           {menuItems.map((item) => (
             <NavLink
               key={item.path}
@@ -60,7 +61,7 @@ const Sidebar = () => {
           <img src="" className="Casual-icon" alt="" />
           <span className="item-label">Выход из системы</span>
         </button>
-        <h6 className="item-label" style={{margin: "18px", color: "var(--text-secondary)"}}>
+        <h6 className={`version-text ${collapsed ? "collapsed" : ""}`}>
           Версия 1.0.2<br />© 2026 Умный Канцеляр
         </h6>
       </div>
