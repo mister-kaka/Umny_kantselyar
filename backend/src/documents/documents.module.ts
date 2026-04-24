b790824d2a2257a1ef857657744726fb85316e65
Александра Москалева <parsivaasol@gmail.com>
2026-04-24 18:25:50 +0300

    проверены все файлы в ./document

--- backend/src/documents/documents.module.ts
+++ backend/src/documents/documents.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DocumentType } from '../entities/document-type.entity';
+import { DocumentCategory } from '../entities/document-category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DocumentType, DocumentCategory])],
   controllers: [DocumentsController],
   providers: [DocumentsService]
 })
