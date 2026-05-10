bf62a63b56340f9420f37154b4b33b85d4a66e98
Mariia Nachinova <mnachinova27@gmail.com>
2026-05-10 15:48:23 +0300

    обновлен ai модуль

--- backend/src/ai/ai.module.ts
+++ backend/src/ai/ai.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { AiController } from './ai.controller';
-// import { AiService } from './ai.service';
+import { HttpModule } from '@nestjs/axios';
+import { AiController } from './ai.controller';
+import { AiService } from './ai.service';
 import { Document } from '../entities/document.entity';
 import { DocumentAiResult } from '../entities/document-ai-result.entity';
 import { AiSetting } from '../entities/ai-setting.entity';
@@ -13,8 +14,9 @@ import { AiSetting } from '../entities/ai-setting.entity';
             DocumentAiResult,
             AiSetting,
         ]),
+        HttpModule,
     ],
-    // controllers: [AiController],
-    // providers: [AiService],
+    controllers: [AiController],
+    providers: [AiService],
 })
 export class AiModule {}
