b6ce1be94ac826b55c0156eb20b37689870f4a13
Mariia Nachinova <mnachinova27@gmail.com>
2026-05-10 10:46:41 +0300

    добавлен AiResultDto

---
+++ backend/src/ai/dto/ai-result.dto.ts
@@ -0,0 +1,12 @@
+export class AiResultResponseDto {
+    id!: number;
+    documentId!: number;
+    documentTypeSuggested!: string | null;
+    categorySuggested!: string | null;
+    summaryText!: string | null;
+    departmentSuggested!: string | null;
+    confidenceScore!: number | null;
+    providerCode!: string;
+    modelName!: string;
+    createdAt!: Date;
+}
