6f763083fea69d820ee3c01b8726ea022058ddf2
Mariia Nachinova <mnachinova27@gmail.com>
2026-05-16 00:23:39 +0300

    добавлены DocumentFile и OcrResult в зависимости модуля

--- backend/src/documents/documents.module.ts
+++ backend/src/documents/documents.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
-
 import { Document } from '../entities/document.entity';
 import { DocumentRoute } from '../entities/document-route.entity';
+import { DocumentFile } from '../entities/document-file.entity';
+import { OcrResult } from '../entities/ocr-result.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([
-        Document,
-        DocumentRoute,
-    ])],
+    imports: [
+        TypeOrmModule.forFeature([
+            Document,
+            DocumentRoute,
+            DocumentFile,
+            OcrResult,
+        ]),
+    ],
     controllers: [DocumentsController],
     providers: [DocumentsService],
 })
