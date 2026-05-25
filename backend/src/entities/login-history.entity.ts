2f0b5e096098a844e29609c6ad36ea8fdc65f330
Mariia Nachinova <mnachinova27@gmail.com>
2026-05-25 14:58:38 +0300

    небольшие правки

--- backend/src/entities/login-history.entity.ts
+++ backend/src/entities/login-history.entity.ts
@@ -9,7 +9,7 @@ export class LoginHistory {
     @Column({ name: 'user_id' })
     userId!: number;
 
-    @Column({ name: 'ip_address', length: 45, nullable: true })
+    @Column({ name: 'ip_address', type: 'varchar', length: 45, nullable: true })
     ipAddress!: string | null;
 
     @Column({ name: 'user_agent', type: 'text', nullable: true })
